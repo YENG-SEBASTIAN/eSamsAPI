aa0e8142df20cf1b21653c8daa4a16ed6f0d6c8f
YENG-SEBASTIAN <yengaebastian1@gmail.com>
2023-09-20 04:11:25 -0700

    vercel

--- eSams/eSams/settings.py
+++ eSams/eSams/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-j83jztl_6_oj%um5^l=s%g@2h+_(*+fu$z90_da4^x$84qdmo^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition

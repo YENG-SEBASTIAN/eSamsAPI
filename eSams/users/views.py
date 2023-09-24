c347a59899f6d453a178f8ed6d897293cd2b6920
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2023-09-24 11:36:05 +0100

    get lecturer api

--- eSams/users/views.py
+++ eSams/users/views.py
@@ -52,6 +52,7 @@ def get_lecturers(request):
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+
 # create a user profile
 class SetProfileInfo(APIView):
     parser_classes = (MultiPartParser, FormParser)

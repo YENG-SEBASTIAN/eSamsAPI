64a39100623d2d0ad0b50d37e155ec314c56a022
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2023-10-25 13:30:37 +0100

    swagger documentation added

--- eSams/eSams/urls.py
+++ eSams/eSams/urls.py
@@ -18,6 +18,23 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view( 
+    openapi.Info(
+        title="Smart Examination Attendance Management System(SEAMS) API",
+        default_version="v1",
+        description="An attendance API using Face Recognition",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="yengsebastians@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +43,9 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('students/', include('students.urls')),
     path('lecturers/', include('lecturers.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
 
 

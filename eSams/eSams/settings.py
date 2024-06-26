58a9535e59e9b96c2918199f0aff44aa0618a1c6
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-06-26 10:40:09 +0200

    db

--- eSams/eSams/settings.py
+++ eSams/eSams/settings.py
@@ -89,6 +89,12 @@ WSGI_APPLICATION = 'eSams.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
